9d1f78cbfc00cbe0498733892f1f9a95baeea6c8
romain-bodiguel <romain.bodiguel@gmail.com>
2022-06-10 16:58:51 +0200

    first commit, installing react from scratch

---
+++ src/components/Spire/index.js
@@ -0,0 +1,15 @@
+// == Import: npm
+// Styles
+import './spire.scss';
+
+// == Composant
+function App() {
+  return (
+    <div className="Spire">
+      <h1>Spire</h1>
+    </div>
+  );
+}
+
+// == Export
+export default App;
